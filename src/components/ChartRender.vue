500cb43096bd36435237d7a1315df1653af5083d
Yoshinori Shimoda <ys@platinum.local>
2022-08-25 06:28:30 +0900

    サンプルデータを初期配置し、methods関数でデータの書き換えを行う確認

--- src/components/ChartRender.vue
+++ src/components/ChartRender.vue
@@ -15,20 +15,8 @@ export default {
   components: { Line },
   methods: {
     importCsv: function () {
-      this.chartData = {
-        datasets: [
-          {
-            label: '日中の眠気強度',
-            backgroundColor: 'rgba(102, 153, 255, 1.0)',
-            borderColor: 'rgba(102, 153, 255, 1.0)',
-            data: {
-              January: 15,
-              April: 25,
-              Jun: 35
-            },
-          }
-        ]
-      }
+      this.chartData.datasets[0].label = 'great!';
+      this.chartData.datasets[0].data.January = '25';
     }
   },
   data() {
@@ -43,28 +31,28 @@ export default {
               January: 100,
               February: 200,
               March: 400
+            }
+          },
+          {
+            label: '日中の眠気強度',
+            backgroundColor: 'rgba(102, 153, 255, 1.0)',
+            borderColor: 'rgba(102, 153, 255, 1.0)',
+            data: {
+              January: 15,
+              April: 25,
+              Jun: 35
             },
           },
-          // {
-          //   label: '日中の眠気強度',
-          //   backgroundColor: 'rgba(102, 153, 255, 1.0)',
-          //   borderColor: 'rgba(102, 153, 255, 1.0)',
-          //   data: {
-          //     January: 15,
-          //     April: 25,
-          //     Jun: 35
-          //   },
-          // },
-          // {
-          //   label: '鼻詰まり強度',
-          //   backgroundColor: 'rgba(102, 255, 102, 1.0)',
-          //   borderColor: 'rgba(102, 255, 102, 1.0)',
-          //   data: {
-          //     July: 3,
-          //     August: 12,
-          //     September: 9
-          //   },
-          // }
+          {
+            label: '鼻詰まり強度',
+            backgroundColor: 'rgba(102, 255, 102, 1.0)',
+            borderColor: 'rgba(102, 255, 102, 1.0)',
+            data: {
+              July: 3,
+              August: 12,
+              September: 9
+            },
+          }
         ]
       }
     }
